facf2cfff8fea403d659f46a03561539688c9956
tehw0lf <tehwolf@protonmail.com>
2024-05-16 18:22:28 +0200

    Initial commit

---
+++ install-wabbajack-cli.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Configuration
+CLI_DOWNLOAD_FILE="$HOME/Downloads/wabbajack-cli.zip"
+NEXUS_API_KEY=""
+WABBAJACK_PREFIX="/data/Wabbajack"
+WABBAJACK_DIR="$WABBAJACK_PREFIX/drive_c/Wabbajack"
+WABBAJACK_VERSION="3.5.0.1" # todo: get current version?
+
+mkdir -p $WABBAJACK_PREFIX
+env WINEPREFIX=$WABBAJACK_PREFIX wineboot -u
+mkdir -p $WABBAJACK_DIR
+unzip $CLI_DOWNLOAD_FILE -d $WABBAJACK_DIR/$WABBAJACK_VERSION
+cd $WABBAJACK_DIR
+chmod +x $WABBAJACK_DIR/$WABBAJACK_VERSION/wabbajack-cli.exe
+# todo: install dotnet8 and reactivate the step below
+#env WINEPREFIX=$WABBAJACK_PREFIX wine $WABBAJACK_DIR/$WABBAJACK_VERSION/wabbajack-cli.exe setnexusapikey $NEXUS_API_KEY
---
+++ install-wabbajack-gui.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Configuration
+GUI_DOWNLOAD_FILE="$HOME/Downloads/Wabbajack.exe"
+WABBAJACK_PREFIX="/data/Wabbajack"
+WABBAJACK_DIR="$WABBAJACK_PREFIX/drive_c/Wabbajack"
+
+mkdir -p $WABBAJACK_PREFIX
+env WINEPREFIX=$WABBAJACK_PREFIX wineboot -u
+env WINEPREFIX=$WABBAJACK_PREFIX winetricks win7
+mkdir -p $WABBAJACK_DIR
+cp $GUI_DOWNLOAD_FILE -d $WABBAJACK_DIR/
+cd $WABBAJACK_DIR
+chmod +x $WABBAJACK_DIR/Wabbajack.exe
+env WINEPREFIX=$WABBAJACK_PREFIX wine $WABBAJACK_DIR/Wabbajack.exe
