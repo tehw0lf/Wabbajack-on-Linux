f3607e06235b65fd504afc400edf3cd74793c3ef
tehw0lf <tehwolf@protonmail.com>
2024-05-16 21:58:13 +0200

    Fix typo and optimize

--- install-wabbajack-gui.sh
+++ install-wabbajack-gui.sh
@@ -2,6 +2,7 @@
 
 # configuration
 GUI_DOWNLOAD_FILE="$HOME/Downloads/Wabbajack.exe"
+EDGE_DOWNLOAD_FILE="$HOME/Downloads/MicrosoftEdgeSetup.exe"
 WABBAJACK_PREFIX="/data/Wabbajack"
 WABBAJACK_DIR="$WABBAJACK_PREFIX/drive_c/Wabbajack"
 MODLIST_DIR="$WABBAJACK_PREFIX/drive_c/Modlists"
@@ -18,11 +19,8 @@ mkdir -p $MODLIST_DIR
 # enable steam library detection
 ln -s "$STEAM_DIR" "$SYMLINK_DIR"
 cp $GUI_DOWNLOAD_FILE -d $WABBAJACK_DIR/
-cd $WABBAJACK_DIR
 chmod +x $WABBAJACK_DIR/Wabbajack.exe
 
 # create run script
-echo "env WINEPREFIX=\"$WABBAJACK_PREFIX\" wine \"$WABBAJACK_DIR/Wabbajack.exe\"" >"$WABBAJACKDIR/run-wabbajack.sh"
-
-# run gui
-env WINEPREFIX=$WABBAJACK_PREFIX wine $WABBAJACK_DIR/Wabbajack.exe
+echo "env WINEPREFIX=\"$WABBAJACK_PREFIX\" wine \"$WABBAJACK_DIR/Wabbajack.exe\"" >"$WABBAJACK_DIR/run-wabbajack.sh"
+chmod +x "$WABBAJACK_DIR/run-wabbajack.sh"
