4e2e595e202f495db15e312006a7344cf6e12af2
tehw0lf <tehwolf@protonmail.com>
2024-05-16 18:27:55 +0200

    Create modlist dir as well

--- install-wabbajack-gui.sh
+++ install-wabbajack-gui.sh
@@ -4,11 +4,13 @@
 GUI_DOWNLOAD_FILE="$HOME/Downloads/Wabbajack.exe"
 WABBAJACK_PREFIX="/data/Wabbajack"
 WABBAJACK_DIR="$WABBAJACK_PREFIX/drive_c/Wabbajack"
+MODLIST_DIR="$WABBAJACK_PREFIX/drive_c/Modlists"
 
 mkdir -p $WABBAJACK_PREFIX
 env WINEPREFIX=$WABBAJACK_PREFIX wineboot -u
 env WINEPREFIX=$WABBAJACK_PREFIX winetricks win7
 mkdir -p $WABBAJACK_DIR
+mkdir -p $MODLIST_DIR
 cp $GUI_DOWNLOAD_FILE -d $WABBAJACK_DIR/
 cd $WABBAJACK_DIR
 chmod +x $WABBAJACK_DIR/Wabbajack.exe
