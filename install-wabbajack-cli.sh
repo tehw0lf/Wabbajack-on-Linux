5ff3ac22633c6ab790f4c9d7bfaa237627dce61e
tehw0lf <tehwolf@protonmail.com>
2024-05-17 11:51:27 +0200

    Optimize scripts
    Add symlink for modlists and downloaded modlists to make the prefix disposable

--- install-wabbajack-cli.sh
+++ install-wabbajack-cli.sh
@@ -5,25 +5,33 @@ WABBAJACK_ZIP="/$HOME/Downloads/wabbajack-cli.zip"
 NEXUS_API_KEY="YOURAPIKEY"
 WABBAJACK_PREFIX="/data/Wabbajack"
 WABBAJACK_DIR="$WABBAJACK_PREFIX/drive_c/Wabbajack"
-MODLIST_DIR="$WABBAJACK_PREFIX/drive_c/Modlists"
+MODLIST_DIR="/data/Modlists"
+MODLIST_SYMLINK_DIR="$WABBAJACK_PREFIX/drive_c/Modlists"
+DOWNLOADED_MODLISTS_DIR="/data/downloaded_mod_lists"
+DOWNLOADED_MODLISTS_SYMLINK_DIR="$WABBAJACK_PREFIX/drive_c/Wabbajack/wabbajack-set-nexus-api-key/downloaded_mod_lists"
 STEAM_DIR="/home/tehwolf/.local/share/Steam"
-SYMLINK_DIR="/data/Wabbajack/drive_c/Program Files (x86)/Steam"
+STEAM_SYMLINK_DIR="/data/Wabbajack/drive_c/Program Files (x86)/Steam"
 
 # prefix
-mkdir -p $WABBAJACK_PREFIX
-env WINEPREFIX=$WABBAJACK_PREFIX wineboot -u
-mkdir -p $WABBAJACK_DIR
-mkdir -p $MODLIST_DIR
+mkdir -p "$WABBAJACK_PREFIX"
+env WINEPREFIX="$WABBAJACK_PREFIX" wineboot -u
+mkdir -p "$WABBAJACK_DIR/wabbajack-set-nexus-api-key"
 
 # enable steam library detection
-ln -s "$STEAM_DIR" "$SYMLINK_DIR"
+ln -s "$STEAM_DIR" "$STEAM_SYMLINK_DIR"
 
-# wabbajack
-unzip $WABBAJACK_ZIP -d $WABBAJACK_DIR/wabbajack-set-nexus-api-key
-chmod +x $WABBAJACK_DIR/wabbajack-set-nexus-api-key/wabbajack-cli.exe
+# symlink modlist folder
+ln -s "$MODLIST_DIR" "$MODLIST_SYMLINK_DIR"
+
+# symlink downloaded_mod_lists folder
+ln -s "$DOWNLOADED_MODLISTS_DIR" "$DOWNLOADED_MODLISTS_SYMLINK_DIR"
+
+# copy wabbajack and make it executable
+unzip "$WABBAJACK_ZIP" -d "$WABBAJACK_DIR/wabbajack-set-nexus-api-key"
+chmod +x "$WABBAJACK_DIR/wabbajack-set-nexus-api-key/wabbajack-cli.exe"
 
 # run cli and set nexus api key to test functionality
-cd $WABBAJACK_DIR
-env WINEPREFIX=$WABBAJACK_PREFIX wine $WABBAJACK_DIR/wabbajack-set-nexus-api-key/wabbajack-cli.exe set-nexus-api-key -k $NEXUS_API_KEY
+cd "$WABBAJACK_DIR"
+env WINEPREFIX="$WABBAJACK_PREFIX" wine "$WABBAJACK_DIR/wabbajack-set-nexus-api-key/wabbajack-cli.exe" set-nexus-api-key -k "$NEXUS_API_KEY"
 
-echo "Installation complete. Use env WINEPREFIX=$WABBAJACK_PREFIX wine $WABBAJACK_DIR/wabbajack-set-nexus-api-key/wabbajack-cli.exe to launch Wabbajack CLI"
+echo "Installation complete. Use env WINEPREFIX=\""$WABBAJACK_PREFIX/"\" wine \""$WABBAJACK_DIR"/wabbajack-set-nexus-api-key/wabbajack-cli.exe\" to launch Wabbajack CLI"
--- install-wabbajack-gui.sh
+++ install-wabbajack-gui.sh
@@ -4,26 +4,34 @@
 GUI_DOWNLOAD_FILE="$HOME/Downloads/Wabbajack.exe"
 WABBAJACK_PREFIX="/data/Wabbajack"
 WABBAJACK_DIR="$WABBAJACK_PREFIX/drive_c/Wabbajack"
-MODLIST_DIR="$WABBAJACK_PREFIX/drive_c/Modlists"
+MODLIST_DIR="/data/Modlists"
+MODLIST_SYMLINK_DIR="$WABBAJACK_PREFIX/drive_c/Modlists"
+DOWNLOADED_MODLISTS_DIR="/data/downloaded_mod_lists"
+DOWNLOADED_MODLISTS_SYMLINK_DIR="$WABBAJACK_PREFIX/drive_c/Wabbajack/3.5.0.1/downloaded_mod_lists"
 STEAM_DIR="/home/tehwolf/.local/share/Steam"
-SYMLINK_DIR="/data/Wabbajack/drive_c/Program Files (x86)/Steam"
+STEAM_SYMLINK_DIR="/data/Wabbajack/drive_c/Program Files (x86)/Steam"
 
 # prefix
-mkdir -p $WABBAJACK_PREFIX
-env WINEPREFIX=$WABBAJACK_PREFIX wineboot -u
-env WINEPREFIX=$WABBAJACK_PREFIX winetricks win7
-mkdir -p $WABBAJACK_DIR
-mkdir -p $MODLIST_DIR
+mkdir -p "$WABBAJACK_PREFIX"
+env WINEPREFIX="$WABBAJACK_PREFIX" wineboot -u
+env WINEPREFIX="$WABBAJACK_PREFIX" winetricks win7
+mkdir -p "$WABBAJACK_DIR/3.5.0.1"
 
 # enable steam library detection
-ln -s "$STEAM_DIR" "$SYMLINK_DIR"
+ln -s "$STEAM_DIR" "$STEAM_SYMLINK_DIR"
 
-# wabbajack
-cp $GUI_DOWNLOAD_FILE -d $WABBAJACK_DIR/
-chmod +x $WABBAJACK_DIR/Wabbajack.exe
+# symlink modlist folder
+ln -s "$MODLIST_DIR" "$MODLIST_SYMLINK_DIR"
 
-# create run script
-echo "env WINEPREFIX=\"$WABBAJACK_PREFIX\" wine \"$WABBAJACK_DIR/Wabbajack.exe\"" >"$WABBAJACK_DIR/run-wabbajack.sh"
+# symlink downloaded_mod_lists folder
+ln -s "$DOWNLOADED_MODLISTS_DIR" "$DOWNLOADED_MODLISTS_SYMLINK_DIR"
+
+# copy wabbajack and make it executable
+cp "$GUI_DOWNLOAD_FILE" -d "$WABBAJACK_DIR/"
+chmod +x "$WABBAJACK_DIR/Wabbajack.exe"
+
+# create run script and make it executable
+echo "env WINEPREFIX=\""$WABBAJACK_PREFIX/"\" wine \""$WABBAJACK_DIR"/Wabbajack.exe\"" >"$WABBAJACK_DIR/run-wabbajack.sh"
 chmod +x "$WABBAJACK_DIR/run-wabbajack.sh"
 
-echo "Installation complete. Use the created run script at $WABBAJACK_DIR/run-wabbajack.su to launch Wabbajack"
+echo "Installation complete. Use the created run script at "$WABBAJACK_DIR/run-wabbajack.sh" to launch Wabbajack"
